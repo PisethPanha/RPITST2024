5424a5c6499f3b7a4329c2c99c8b9743a441a9b6
PisethPanha <100303620+PisethPanha@users.noreply.github.com>
2024-10-19 17:02:55 +0700

    Update index-Br_3flKo.js

--- assets/index-Br_3flKo.js
+++ assets/index-Br_3flKo.js
@@ -31700,7 +31700,7 @@ function Ce(e) {
       h.jsx("center", {
         children: h.jsx("img", {
           className: "w-[200px] self-center rounded-lg",
-          src: `https://rpitst2024.great-site.net/${e.img}`,
+          src: `https://corsproxy.io/?https%3A%2F%2Frpitst2024.great-site.net%2F${e.img}`,
         }),
       }),
       h.jsx("div", {
